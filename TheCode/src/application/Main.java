4352105af226c99fe9aebc869c53b10ceca66069
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-12-21 08:10:13 +0100

    Update mainwindow so it cant be resized

--- TheCode/src/application/Main.java
+++ TheCode/src/application/Main.java
@@ -1,26 +1,26 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("frame/MainFrame.fxml"));
-			Scene scene = new Scene(root,1440,1080);
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("frame/MainFrame.fxml"));
+			Scene scene = new Scene(root, 1440, 1080);
 			scene.getStylesheets().add(getClass().getResource("util/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
