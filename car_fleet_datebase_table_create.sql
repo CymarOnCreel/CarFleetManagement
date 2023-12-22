42de534a3380d5d61c2aaf442e9b7bfd898c234d
zdomiter <128590130+zdomiter@users.noreply.github.com>
2023-12-22 22:14:12 +0100

    Model kapcsolat javitasa

--- car_fleet_datebase_table_create.sql
+++ car_fleet_datebase_table_create.sql
@@ -1,36 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Gép: localhost
--- Létrehozás ideje: 2023. Dec 22. 18:09
--- Kiszolgáló verziója: 10.3.32-MariaDB
--- PHP verzió: 8.0.23
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Adatbázis: `car_fleet`
---
-CREATE SCHEMA `carfleet_manager` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci ;
--- --------------------------------------------------------
-use `carfleet_manager`;
---
--- Tábla szerkezet ehhez a táblához `car`
---
+CREATE DATABASE IF NOT EXISTS `carfleet_manager` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `carfleet_manager`;
 
 CREATE TABLE `car` (
   `license_plate` varchar(20) COLLATE utf8_hungarian_ci NOT NULL,
   `make` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
-  `model` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `category` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `fuel` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `doors` int(11) NOT NULL,
@@ -46,21 +27,9 @@ CREATE TABLE `car` (
   `enabled` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `category`
---
-
 CREATE TABLE `category` (
   `name_category` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `employee`
---
 
 CREATE TABLE `employee` (
   `id_employee` int(11) NOT NULL,
@@ -75,21 +44,9 @@ CREATE TABLE `employee` (
   `enabled` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `fuel`
---
-
 CREATE TABLE `fuel` (
   `fuel_type` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `insurance`
---
 
 CREATE TABLE `insurance` (
   `id_insurance` int(11) NOT NULL,
@@ -104,21 +61,9 @@ CREATE TABLE `insurance` (
   `enabled` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `insurance_type`
---
-
 CREATE TABLE `insurance_type` (
   `name_insurance_type` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `maintenance`
---
 
 CREATE TABLE `maintenance` (
   `id_maintenance` int(11) NOT NULL,
@@ -133,52 +78,23 @@ CREATE TABLE `maintenance` (
   `updated_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `maintenance_type`
---
-
 CREATE TABLE `maintenance_type` (
   `name_maintenance_type` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `make`
---
 
 CREATE TABLE `make` (
   `name_make` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `model`
---
-
 CREATE TABLE `model` (
-  `name_model` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
+  `name_model` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
+  `make` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `picture`
---
 
 CREATE TABLE `picture` (
   `picture_path` varchar(200) COLLATE utf8_hungarian_ci NOT NULL,
   `license_plate` varchar(20) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `reservation`
---
 
 CREATE TABLE `reservation` (
   `id_reservation` int(11) NOT NULL,
@@ -191,19 +107,10 @@ CREATE TABLE `reservation` (
   `updated_at` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `role`
---
-
 CREATE TABLE `role` (
   `name_role` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
   `description` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
---
--- Tábla szerkezet ehhez a táblához `service_company`
---
 
 CREATE TABLE `service_company` (
   `name_service_company` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
@@ -215,12 +122,6 @@ CREATE TABLE `service_company` (
   `updated_at` date DEFAULT NULL,
   `enabled` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `site`
---
 
 CREATE TABLE `site` (
   `name_site` varchar(45) COLLATE utf8_hungarian_ci NOT NULL,
@@ -235,73 +136,42 @@ CREATE TABLE `site` (
   `enabled` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
--- --------------------------------------------------------
-
---
--- Tábla szerkezet ehhez a táblához `transmission`
---
-
 CREATE TABLE `transmission` (
   `transmission_type` varchar(45) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
---
--- Indexek a kiírt táblákhoz
---
 
---
--- A tábla indexei `car`
---
 ALTER TABLE `car`
   ADD PRIMARY KEY (`license_plate`),
   ADD UNIQUE KEY `license_plate_UNIQUE` (`license_plate`),
   ADD KEY `transmission_idx` (`transmission_type`),
   ADD KEY `make_idx` (`make`),
-  ADD KEY `model_idx` (`model`),
   ADD KEY `category_idx` (`category`),
   ADD KEY `engine_idx` (`fuel`);
 
---
--- A tábla indexei `category`
---
 ALTER TABLE `category`
   ADD PRIMARY KEY (`name_category`),
   ADD UNIQUE KEY `name_category_UNIQUE` (`name_category`);
 
---
--- A tábla indexei `employee`
---
 ALTER TABLE `employee`
   ADD PRIMARY KEY (`id_employee`),
   ADD UNIQUE KEY `id_employee_UNIQUE` (`id_employee`),
   ADD KEY `role_idx` (`role_name`);
 
---
--- A tábla indexei `fuel`
---
 ALTER TABLE `fuel`
   ADD PRIMARY KEY (`fuel_type`),
   ADD UNIQUE KEY `engine_type_UNIQUE` (`fuel_type`);
 
---
--- A tábla indexei `insurance`
---
 ALTER TABLE `insurance`
   ADD PRIMARY KEY (`id_insurance`),
   ADD UNIQUE KEY `id_insurance_UNIQUE` (`id_insurance`),
   ADD KEY `insurance_type_idx` (`insurance_type`),
   ADD KEY `license_plate_idx` (`license_plate`);
 
---
--- A tábla indexei `insurance_type`
---
 ALTER TABLE `insurance_type`
   ADD PRIMARY KEY (`name_insurance_type`),
   ADD UNIQUE KEY `name_insurance_type_UNIQUE` (`name_insurance_type`);
 
---
--- A tábla indexei `maintenance`
---
 ALTER TABLE `maintenance`
   ADD PRIMARY KEY (`id_maintenance`),
   ADD UNIQUE KEY `id_maintenance_UNIQUE` (`id_maintenance`),
@@ -309,144 +179,84 @@ ALTER TABLE `maintenance`
   ADD KEY `main-type` (`maintenance_type`),
   ADD KEY `main-service` (`service_company`);
 
---
--- A tábla indexei `maintenance_type`
---
 ALTER TABLE `maintenance_type`
   ADD PRIMARY KEY (`name_maintenance_type`),
   ADD UNIQUE KEY `name_maintenance_type_UNIQUE` (`name_maintenance_type`);
 
---
--- A tábla indexei `make`
---
 ALTER TABLE `make`
   ADD PRIMARY KEY (`name_make`),
   ADD UNIQUE KEY `make_name_UNIQUE` (`name_make`);
 
---
--- A tábla indexei `model`
---
 ALTER TABLE `model`
   ADD PRIMARY KEY (`name_model`),
-  ADD UNIQUE KEY `name_model_UNIQUE` (`name_model`);
+  ADD UNIQUE KEY `name_model_UNIQUE` (`name_model`),
+  ADD KEY `make-model` (`make`);
 
---
--- A tábla indexei `picture`
---
 ALTER TABLE `picture`
   ADD PRIMARY KEY (`picture_path`),
   ADD UNIQUE KEY `picture_path_UNIQUE` (`picture_path`),
   ADD KEY `pic-car` (`license_plate`);
 
---
--- A tábla indexei `reservation`
---
 ALTER TABLE `reservation`
   ADD PRIMARY KEY (`id_reservation`),
   ADD UNIQUE KEY `id_reservation_UNIQUE` (`id_reservation`),
   ADD KEY `id_employee_id` (`id_employee`),
   ADD KEY `car-reservation_idx` (`license_plate`);
 
---
--- A tábla indexei `role`
---
 ALTER TABLE `role`
   ADD PRIMARY KEY (`name_role`),
   ADD UNIQUE KEY `name_role_UNIQUE` (`name_role`);
 
---
--- A tábla indexei `service_company`
---
 ALTER TABLE `service_company`
   ADD PRIMARY KEY (`name_service_company`),
   ADD UNIQUE KEY `id_service_UNIQUE` (`name_service_company`);
 
---
--- A tábla indexei `site`
---
 ALTER TABLE `site`
   ADD PRIMARY KEY (`name_site`),
   ADD UNIQUE KEY `name_site_UNIQUE` (`name_site`);
 
---
--- A tábla indexei `transmission`
---
 ALTER TABLE `transmission`
   ADD PRIMARY KEY (`transmission_type`),
   ADD UNIQUE KEY `transmission_type_UNIQUE` (`transmission_type`);
 
---
--- A kiírt táblák AUTO_INCREMENT értéke
---
 
---
--- AUTO_INCREMENT a táblához `employee`
---
 ALTER TABLE `employee`
   MODIFY `id_employee` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT a táblához `insurance`
---
 ALTER TABLE `insurance`
   MODIFY `id_insurance` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT a táblához `maintenance`
---
 ALTER TABLE `maintenance`
   MODIFY `id_maintenance` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT a táblához `reservation`
---
 ALTER TABLE `reservation`
   MODIFY `id_reservation` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Megkötések a kiírt táblákhoz
---
 
---
--- Megkötések a táblához `car`
---
 ALTER TABLE `car`
   ADD CONSTRAINT `category` FOREIGN KEY (`category`) REFERENCES `category` (`name_category`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fuel` FOREIGN KEY (`fuel`) REFERENCES `fuel` (`fuel_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `make` FOREIGN KEY (`make`) REFERENCES `make` (`name_make`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `model` FOREIGN KEY (`model`) REFERENCES `model` (`name_model`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `transmission` FOREIGN KEY (`transmission_type`) REFERENCES `transmission` (`transmission_type`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Megkötések a táblához `employee`
---
 ALTER TABLE `employee`
   ADD CONSTRAINT `role` FOREIGN KEY (`role_name`) REFERENCES `role` (`name_role`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Megkötések a táblához `insurance`
---
 ALTER TABLE `insurance`
   ADD CONSTRAINT `insurance_type` FOREIGN KEY (`insurance_type`) REFERENCES `insurance_type` (`name_insurance_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `license_plate` FOREIGN KEY (`license_plate`) REFERENCES `car` (`license_plate`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Megkötések a táblához `maintenance`
---
 ALTER TABLE `maintenance`
   ADD CONSTRAINT `car-main` FOREIGN KEY (`license_plate`) REFERENCES `car` (`license_plate`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `main-service` FOREIGN KEY (`service_company`) REFERENCES `service_company` (`name_service_company`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `main-type` FOREIGN KEY (`maintenance_type`) REFERENCES `maintenance_type` (`name_maintenance_type`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Megkötések a táblához `picture`
---
+ALTER TABLE `model`
+  ADD CONSTRAINT `make-model` FOREIGN KEY (`make`) REFERENCES `make` (`name_make`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 ALTER TABLE `picture`
   ADD CONSTRAINT `pic-car` FOREIGN KEY (`license_plate`) REFERENCES `car` (`license_plate`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
---
--- Megkötések a táblához `reservation`
---
 ALTER TABLE `reservation`
   ADD CONSTRAINT `car-reservation` FOREIGN KEY (`license_plate`) REFERENCES `car` (`license_plate`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `employee-reservation` FOREIGN KEY (`id_employee`) REFERENCES `employee` (`id_employee`) ON DELETE NO ACTION ON UPDATE NO ACTION;
--- insert_hungarian_dummy_data.sql
+++ insert_hungarian_dummy_data.sql
@@ -1,95 +1,83 @@
--- Tábla `category` adatok
-INSERT INTO `category` (`name_category`) VALUES
-('Szédán'),
-('SUV'),
-('Teherautó'),
-('Kisbusz');
 
--- Tábla `fuel` adatok
+INSERT INTO `category` (`name_category`) VALUES
+('Kisbusz'),
+('SUV'),
+('Szédán'),
+('Teherautó');
+
+
 INSERT INTO `fuel` (`fuel_type`) VALUES
 ('Benzin'),
 ('Dízel'),
 ('Elektromos'),
 ('Hibrid');
 
--- Tábla `make` adatok
+
+INSERT INTO `insurance_type` (`name_insurance_type`) VALUES
+('Casco'),
+('Kiterjedt biztosítás'),
+('Kötelező felelőségbiztosítás');
+
+
 INSERT INTO `make` (`name_make`) VALUES
-('Toyota'),
-('Honda'),
+('Chevrolet'),
 ('Ford'),
-('Chevrolet');
+('Honda'),
+('Toyota');
 
--- Tábla `model` adatok
-INSERT INTO `model` (`name_model`) VALUES
-('Camry'),
-('Civic'),
-('F-150'),
-('Silverado');
+INSERT INTO `model` (`name_model`, `make`) VALUES
+('Silverado', 'Chevrolet'),
+('F-150', 'Ford'),
+('Civic', 'Honda'),
+('Camry', 'Toyota');
 
--- Tábla `transmission` adatok
-INSERT INTO `transmission` (`transmission_type`) VALUES
-('Automata'),
-('Kézi');
--
--- A tábla adatainak kiíratása `role`
 
 INSERT INTO `role` (`name_role`, `description`) VALUES
 ('admin', 'mindenre van jogosultsága kivéve a felhasználó létrehozása'),
 ('superadmin', 'mindenre van jogosultsága'),
 ('user', 'lekérdezés jellegű funkciók');
 
+INSERT INTO `service_company` (`name_service_company`, `location`, `contact_person`, `contact_email`, `contact_phone`, `created_at`, `updated_at`, `enabled`) VALUES
+('AutoCare', 'Budapest, Váci utca 1', 'János Kovács', 'janos.kovacs@autocare.hu', '+36-1-234-5678', '2023-12-22', NULL, 1),
+('Speedy Repairs', 'Budapest, Rákóczi út 10', 'Judit Szabó', 'judit.szabo@speedyrepairs.hu', '+36-1-876-5432', '2023-12-22', NULL, 1);
 
--- Tábla `service_company` adatok
-INSERT INTO `service_company` (`name_service_company`, `location`, `contact_person`, `contact_email`, `contact_phone`, `created_at`) VALUES
-('AutoCare', 'Budapest, Váci utca 1', 'János Kovács', 'janos.kovacs@autocare.hu', '+36-1-234-5678', '2023-12-22'),
-('Speedy Repairs', 'Budapest, Rákóczi út 10', 'Judit Szabó', 'judit.szabo@speedyrepairs.hu', '+36-1-876-5432', '2023-12-22');
+INSERT INTO `site` (`name_site`, `location`, `capacity`, `contact_person`, `contact_email`, `contact_phone`, `description`, `created_at`, `updated_at`, `enabled`) VALUES
+('Iroda', 'Budapest, Deák tér 5', 50, 'Gábor Nagy', 'gabor.nagy@office.hu', '+36-1-345-6789', 'Az iroda leírása 1', '2023-12-22', NULL, 1),
+('Raktár', 'Budapest, Blaha Lujza tér 7', 75, 'Katalin Kiss', 'katalin.kiss@warehouse.hu', '+36-1-987-6543', 'A raktár leírása 2', '2023-12-22', NULL, 1);
 
+INSERT INTO `transmission` (`transmission_type`) VALUES
+('Automata'),
+('Kézi');
 
--- Tábla `site` adatok
-INSERT INTO `site` (`name_site`, `location`, `capacity`, `contact_person`, `contact_email`, `contact_phone`, `description`, `created_at`) VALUES
-('Iroda', 'Budapest, Deák tér 5', 50, 'Gábor Nagy', 'gabor.nagy@office.hu', '+36-1-345-6789', 'Az iroda leírása 1', '2023-12-22'),
-('Raktár', 'Budapest, Blaha Lujza tér 7', 75, 'Katalin Kiss', 'katalin.kiss@warehouse.hu', '+36-1-987-6543', 'A raktár leírása 2', '2023-12-22');
--- Tábla `insurance_type` adatok
-INSERT INTO `insurance_type` (`name_insurance_type`) VALUES
-('Kötelező felelőségbiztosítás'),
-('Casco'),
-('Kiterjedt biztosítás');
+INSERT INTO `car` (`license_plate`, `make`, `category`, `fuel`, `doors`, `seats`, `transmission_type`, `mileage`, `service_interval`, `inspection_expiry_date`, `site_name`, `status`, `created_at`, `updated_at`, `enabled`) VALUES
+('ABC123', 'Toyota', 'Szédán', 'Benzin', 4, 5, 'Automata', 50000, 10000, '2024-12-22', 'Iroda', 1, '2023-12-22', NULL, 1),
+('DEF456', 'Ford', 'Teherautó', 'Dízel', 2, 3, 'Automata', 70000, 15000, '2024-12-22', 'Iroda', 1, '2023-12-22', NULL, 1),
+('GHI789', 'Chevrolet', 'Teherautó', 'Benzin', 2, 3, 'Kézi', 80000, 18000, '2024-12-22', 'Raktár', 1, '2023-12-22', NULL, 1),
+('XYZ789', 'Honda', 'Szédán', 'Benzin', 4, 5, 'Kézi', 60000, 12000, '2024-12-22', 'Raktár', 1, '2023-12-22', NULL, 1);
 
--- Tábla `car` adatok
-INSERT INTO `car` (`license_plate`, `make`, `model`, `category`, `fuel`, `doors`, `seats`, `transmission_type`, `mileage`, `service_interval`, `inspection_expiry_date`, `site_name`, `status`, `created_at`, `updated_at`, `enabled`) VALUES
-('ABC123', 'Toyota', 'Camry', 'Szédán', 'Benzin', 4, 5, 'Automata', 50000, 10000, '2024-12-22', 'Iroda', 1, '2023-12-22', NULL, 1),
-('XYZ789', 'Honda', 'Civic', 'Szédán', 'Benzin', 4, 5, 'Kézi', 60000, 12000, '2024-12-22', 'Raktár', 1, '2023-12-22', NULL, 1),
-('DEF456', 'Ford', 'F-150', 'Teherautó', 'Dízel', 2, 3, 'Automata', 70000, 15000, '2024-12-22', 'Iroda', 1, '2023-12-22', NULL, 1),
-('GHI789', 'Chevrolet', 'Silverado', 'Teherautó', 'Benzin', 2, 3, 'Kézi', 80000, 18000, '2024-12-22', 'Raktár', 1, '2023-12-22', NULL, 1);
+INSERT INTO `employee` (`id_employee`, `last_name`, `first_name`, `email`, `password`, `driver_license`, `role_name`, `created_at`, `updated_at`, `enabled`) VALUES
+(1, 'Teszt', 'Elek', 'teszt.elek@pelda.com', 'jelszo123', 'DL12345', 'admin', '2023-12-22', NULL, 1),
+(2, 'Nap', 'Pali', 'nap.pali@pelda.com', 'jelszo456', 'DL67890', 'user', '2023-12-22', NULL, 1),
+(3, 'Super', 'admin', 'super.admin@pelda.com', 'superadmin', 'DL67890', 'superadmin', '2023-12-22', NULL, 1);
 
--- Tábla `employee` adatok
-INSERT INTO `employee` (`last_name`, `first_name`, `email`, `password`, `driver_license`, `role_name`, `created_at`, `updated_at`, `enabled`) VALUES
-('Teszt', 'Elek', 'teszt.elek@pelda.com', 'jelszo123', 'DL12345', 'admin', '2023-12-22', NULL, 1),
-('Nap', 'Pali', 'nap.pali@pelda.com', 'jelszo456', 'DL67890', 'user', '2023-12-22', NULL, 1),
-('Super', 'admin', 'super.admin@pelda.com', 'superadmin', 'DL67890', 'superadmin', '2023-12-22', NULL, 1);
+INSERT INTO `insurance` (`id_insurance`, `license_plate`, `insurance_type`, `insurer_name`, `price`, `expire_date`, `pay_period`, `created_at`, `updated_at`, `enabled`) VALUES
+(1, 'ABC123', 'Kötelező felelőségbiztosítás', 'BiztosítóCég1', 500, '2024-12-31', 'Éves', '2023-12-22', NULL, 1),
+(2, 'XYZ789', 'Casco', 'BiztosítóCég2', 700, '2024-12-31', 'Fél éves', '2023-12-22', NULL, 1),
+(3, 'DEF456', 'Kiterjedt biztosítás', 'BiztosítóCég3', 1000, '2024-12-31', 'Havi', '2023-12-22', NULL, 1),
+(4, 'GHI789', 'Kötelező felelőségbiztosítás', 'BiztosítóCég4', 600, '2024-12-31', 'Éves', '2023-12-22', NULL, 1);
 
--- Tábla `insurance` adatok
-INSERT INTO `insurance` (`license_plate`, `insurance_type`, `insurer_name`, `price`, `expire_date`, `pay_period`, `created_at`, `updated_at`, `enabled`) VALUES
-('ABC123', 'Kötelező felelőségbiztosítás', 'BiztosítóCég1', 500, '2024-12-31', 'Éves', '2023-12-22', NULL, 1),
-('XYZ789', 'Casco', 'BiztosítóCég2', 700, '2024-12-31', 'Fél éves', '2023-12-22', NULL, 1),
-('DEF456', 'Kiterjedt biztosítás', 'BiztosítóCég3', 1000, '2024-12-31', 'Havi', '2023-12-22', NULL, 1),
-('GHI789', 'Kötelező felelőségbiztosítás', 'BiztosítóCég4', 600, '2024-12-31', 'Éves', '2023-12-22', NULL, 1);
-
--- Tábla `maintenance_type` adatok
 INSERT INTO `maintenance_type` (`name_maintenance_type`) VALUES
-('Olajcsere'),
 ('Fékellenőrzés'),
 ('Gumikerekek forgatása'),
-('Motorkarbantartás');
+('Motorkarbantartás'),
+('Olajcsere');
 
--- Tábla `maintenance` adatok
-INSERT INTO `maintenance` (`license_plate`, `maintenance_type`, `service_company`, `date`, `mileage`, `description`, `amount`, `created_at`, `updated_at`) VALUES
-('ABC123', 'Olajcsere', 'AutoCare', '2023-12-23', 51000, 'Rendszeres olajcsere', 50, '2023-12-22', NULL),
-('XYZ789', 'Fékellenőrzés', 'Speedy Repairs', '2023-12-24', 62000, 'Fékrendszer ellenőrzés', 80, '2023-12-22', NULL),
-('DEF456', 'Gumikerekek forgatása', 'AutoCare', '2023-12-25', 72000, 'Gumik forgatása egyenletes kopás érdekében', 40, '2023-12-22', NULL),
-('GHI789', 'Motorkarbantartás', 'Speedy Repairs', '2023-12-26', 82000, 'Teljes motorkarbantartás', 120, '2023-12-22', NULL);
+INSERT INTO `maintenance` (`id_maintenance`, `license_plate`, `maintenance_type`, `service_company`, `date`, `mileage`, `description`, `amount`, `created_at`, `updated_at`) VALUES
+(1, 'ABC123', 'Olajcsere', 'AutoCare', '2023-12-23', 51000, 'Rendszeres olajcsere', 50, '2023-12-22', NULL),
+(2, 'XYZ789', 'Fékellenőrzés', 'Speedy Repairs', '2023-12-24', 62000, 'Fékrendszer ellenőrzés', 80, '2023-12-22', NULL),
+(3, 'DEF456', 'Gumikerekek forgatása', 'AutoCare', '2023-12-25', 72000, 'Gumik forgatása egyenletes kopás érdekében', 40, '2023-12-22', NULL),
+(4, 'GHI789', 'Motorkarbantartás', 'Speedy Repairs', '2023-12-26', 82000, 'Teljes motorkarbantartás', 120, '2023-12-22', NULL);
 
--- Tábla `reservation` adatok
 INSERT INTO `reservation` (`id_reservation`, `id_employee`, `license_plate`, `start_date_time`, `end_date_time`, `description`, `created_at`, `updated_at`) VALUES
 (1, 1, 'ABC123', '2023-12-23 08:00:00', '2023-12-23 17:00:00', 'Ügyfél találkozó', '2023-12-22', NULL),
 (2, 2, 'XYZ789', '2023-12-24 10:30:00', '2023-12-24 15:30:00', 'Bevásárlás', '2023-12-22', NULL),
