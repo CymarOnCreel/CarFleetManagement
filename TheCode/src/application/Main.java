46a174983e7945892b6704df80de7aba973aab0f
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2023-12-26 12:03:48 +0100

    Designed mainpage inserted
    - the designed fxml page polished and inserted in the application

--- TheCode/src/application/Main.java
+++ TheCode/src/application/Main.java
@@ -3,18 +3,22 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("frame/MainFrame.fxml"));
-			Scene scene = new Scene(root, 1440, 1080);
+			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("frame/Home.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("util/application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setMaximized(true);
 			primaryStage.setResizable(false);
+			primaryStage.setTitle("Car Fleet Management");
+			primaryStage.getIcons().add(new Image("application/pictures/logo.png"));
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
