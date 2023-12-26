046aace87209ed38ab6c8ee628abe40a58c38b15
zdomiter <zdomiter@gmail.com>
2023-12-26 17:48:06 +0100

    restore

--- TheCode/src/application/alert/AlertMessage.java
+++ TheCode/src/application/alert/AlertMessage.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
 public class AlertMessage {
+<<<<<<< Updated upstream
 
 	public void numberFormatAlert() {
 		Alert a = new Alert(AlertType.NONE, "Ebbe a mezőbe csak számot lehet megadni!!!", ButtonType.OK);
@@ -18,9 +19,22 @@ public class AlertMessage {
 		a.show();
 	}
 
+=======
+	
+>>>>>>> Stashed changes
 	public void emptyTextFieldAlert() {
 		Alert a = new Alert(AlertType.NONE, "Nem lett minden kötelező mező kitöltve!", ButtonType.OK);
 		a.setTitle("Hiányzó adat!!!");
 		a.show();
 	}
+<<<<<<< Updated upstream
+=======
+	
+	public void saveToDatabaseAlert() {
+		Alert a = new Alert(AlertType.NONE, "Valami hiba történt a mentés során!", ButtonType.OK);
+		a.setTitle("Hiba!!!");
+		a.show();
+	}
+
+>>>>>>> Stashed changes
 }
--- TheCode/src/application/dto/SiteDto.java
+++ TheCode/src/application/dto/SiteDto.java
@@ -2,4 +2,10 @@ package application.dto;
 
 public class SiteDto {
 
+	//ez a getter már kellett a CarNewFrameController-ben, azért hoztam létre.
+	public String getNameModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
