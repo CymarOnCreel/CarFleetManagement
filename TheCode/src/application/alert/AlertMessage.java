d57bb41da4e27aa7a5ef419bb5161cb595b7850e
zdomiter <zdomiter@gmail.com>
2023-12-26 17:52:27 +0100

    restore_Alert

--- TheCode/src/application/alert/AlertMessage.java
+++ TheCode/src/application/alert/AlertMessage.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 
 public class AlertMessage {
-<<<<<<< Updated upstream
 
 	public void numberFormatAlert() {
 		Alert a = new Alert(AlertType.NONE, "Ebbe a mezőbe csak számot lehet megadni!!!", ButtonType.OK);
@@ -19,22 +18,10 @@ public class AlertMessage {
 		a.show();
 	}
 
-=======
-	
->>>>>>> Stashed changes
 	public void emptyTextFieldAlert() {
 		Alert a = new Alert(AlertType.NONE, "Nem lett minden kötelező mező kitöltve!", ButtonType.OK);
 		a.setTitle("Hiányzó adat!!!");
 		a.show();
 	}
-<<<<<<< Updated upstream
-=======
-	
-	public void saveToDatabaseAlert() {
-		Alert a = new Alert(AlertType.NONE, "Valami hiba történt a mentés során!", ButtonType.OK);
-		a.setTitle("Hiba!!!");
-		a.show();
-	}
 
->>>>>>> Stashed changes
 }
