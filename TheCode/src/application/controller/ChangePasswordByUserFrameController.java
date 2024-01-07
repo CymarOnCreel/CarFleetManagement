f184fdebc711c57deeb83fd53ede1d7836727f08
Tamás Zoltán <96738810+CymarOnCreel@users.noreply.github.com>
2024-01-07 17:20:30 +0100

    update passwordchange
    added styling
    updated usergiven text to hungarian

--- TheCode/src/application/controller/ChangePasswordByUserFrameController.java
+++ TheCode/src/application/controller/ChangePasswordByUserFrameController.java
@@ -37,17 +37,15 @@ public class ChangePasswordByUserFrameController implements Initializable {
 	@FXML
 	private PasswordField oldPasswordTf;
 
-	private int userId; 
+	private int userId;
 	private EmployeeDao employeeDao;
 	private EmployeeDto employeeLoggedIn;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		employeeDao = new EmployeeDao();
-		userId= UserSession.getUserId();
-		System.out.println(userId);
-		employeeLoggedIn=employeeDao.findById(userId);
-		System.out.println(employeeLoggedIn.toString());
+		userId = UserSession.getUserId();
+		employeeLoggedIn = employeeDao.findById(userId);
 	}
 
 	@FXML
@@ -60,24 +58,26 @@ public class ChangePasswordByUserFrameController implements Initializable {
 	@FXML
 	private void changePassword(ActionEvent event) {
 		String oldPassword = DigestUtils.sha1Hex(oldPasswordTf.getText());
-		System.out.println(oldPassword);
 		String newPasswordFirstTime = newPasswordFirst.getText();
 		String newPasswordSecondTime = newPasswordSecond.getText();
 		if (areTextFieldsFilled()) {
 			if (isOldPasswordCorrect(oldPassword)
-					&& isNewPasswordFirstSameAsNewPasswordSecond(newPasswordFirstTime, newPasswordSecondTime)) {
+					&& isNewPasswordFirstSameAsNewPasswordSecond(newPasswordFirstTime, newPasswordSecondTime)
+					&& !isOldPasswordAndNewPasswordSame(oldPassword, newPasswordFirstTime, newPasswordSecondTime)) {
 				updateUsersPassword(newPasswordFirstTime);
 			} else if (!isOldPasswordCorrect(oldPassword)
 					&& !isNewPasswordFirstSameAsNewPasswordSecond(newPasswordFirstTime, newPasswordSecondTime)) {
-				sendErrorAllDataIsWrong();
+				sendErrorAllDataIncorrect();
 			} else if (!isNewPasswordFirstSameAsNewPasswordSecond(newPasswordFirstTime, newPasswordSecondTime)) {
 
 				sendErrorAboutNewPasswordsNotMatchToUser();
 			} else if (!isOldPasswordCorrect(oldPassword)) {
 				sendErrorAboutOldPasswordNotMatchToUser();
+			} else if (isOldPasswordAndNewPasswordSame(oldPassword, newPasswordFirstTime, newPasswordSecondTime)) {
+				sendErrorOldAndNewPasswordCannotBeSame();
 			}
 		} else {
-			sendErrorAllDataIsWrong();
+			sendErrorFieldsEmpty();
 		}
 	}
 
@@ -94,45 +94,80 @@ public class ChangePasswordByUserFrameController implements Initializable {
 	private void updateUsersPassword(String password) {
 		String newPassword = DigestUtils.sha1Hex(password);
 		employeeLoggedIn.setPassword(newPassword);
-		System.out.println(employeeLoggedIn.toString());
-		employeeDao=new EmployeeDao();
+		employeeDao = new EmployeeDao();
 		employeeDao.update(employeeLoggedIn);
-		new AlertMessage().showConfirmationAlertMessage("Password Changed", "Your password changed succesfully");
+		new AlertMessage().showConfirmationAlertMessage("Jelszó módositva", "A jelszó sikeresen módositva :)");
 		closeWindow();
 	}
 
-	private void sendErrorAllDataIsWrong() {
-		new AlertMessage().showUnknownError("All Data Error",
-				"Old Password is not correct \nNew passwords must match!!!");
-		newPasswordFirst.setStyle("wrong-input-background");
-		newPasswordSecond.setStyle("wrong-input-background");
-		oldPasswordTf.setStyle("wrong-input-background");
+	private void sendErrorAllDataIncorrect() {
+		new AlertMessage().showUnknownError("Adatok nem megfelelőek",
+				"Az új jelszavaknak meg kell egyezniük!!!\nA régi jelszó nem megfelelő!!!");
+		addWrongDastaStyle(newPasswordFirst);
+		addWrongDastaStyle(newPasswordSecond);
+		addWrongDastaStyle(oldPasswordTf);
+
+	}
+
+	private void sendErrorFieldsEmpty() {
+		new AlertMessage().showUnknownError("Üres mezők", "Mezők kitöltése kötelező!!!");
+		if (newPasswordFirst.getText().isEmpty() || newPasswordFirst.getText().length() == 0) {
+			addWrongDastaStyle(newPasswordFirst);
+		} else {
+			removeWrongDastaStyle(newPasswordFirst);
+		}
+		if (newPasswordSecond.getText().isEmpty() || newPasswordSecond.getText().length() == 0) {
+			addWrongDastaStyle(newPasswordSecond);
+		} else {
+			removeWrongDastaStyle(newPasswordSecond);
+		}
+		if (oldPasswordTf.getText().isEmpty() || oldPasswordTf.getText().length() == 0) {
+			addWrongDastaStyle(oldPasswordTf);
+		} else {
+			removeWrongDastaStyle(oldPasswordTf);
+		}
 	}
 
 	private void sendErrorAboutNewPasswordsNotMatchToUser() {
-		new AlertMessage().showUnknownError("Password dont match", "New passwords must match!!!");
-		oldPasswordTf.getStyleClass().remove("wrong-input-background");
-		newPasswordFirst.setStyle("wrong-input-background");
-		newPasswordSecond.setStyle("wrong-input-background");
+		new AlertMessage().showUnknownError("Új jelszó hiba", "Az új jelszavaknak egyezniük kell!!!");
+		removeWrongDastaStyle(oldPasswordTf);
+		addWrongDastaStyle(newPasswordFirst);
+		addWrongDastaStyle(newPasswordSecond);
 	}
 
 	private void sendErrorAboutOldPasswordNotMatchToUser() {
-		new AlertMessage().showUnknownError("Password dont match", "The old password is not correct!!!");
-		oldPasswordTf.setStyle("wrong-input-background");
-		newPasswordFirst.getStyleClass().remove("wrong-input-background");
-		newPasswordSecond.getStyleClass().remove("wrong-input-background");
+		new AlertMessage().showUnknownError("Régi jelszó hiba", "A régi jelszó hibás!!!");
+		addWrongDastaStyle(oldPasswordTf);
+		removeWrongDastaStyle(newPasswordFirst);
+		removeWrongDastaStyle(newPasswordSecond);
+	}
+
+	private void sendErrorOldAndNewPasswordCannotBeSame() {
+		new AlertMessage().showUnknownError("Azonos jelszó", "Az új jelszó nem lehet a régi jelszó!.");
+		addWrongDastaStyle(oldPasswordTf);
+		addWrongDastaStyle(newPasswordFirst);
+		addWrongDastaStyle(newPasswordSecond);
 	}
 
 	private boolean isOldPasswordCorrect(String oldPassword) {
-		System.out.println("oldpassw "+employeeLoggedIn.getPassword());
-		System.out.println(DigestUtils.sha1Hex(oldPasswordTf.getText()));
-		System.out.println(employeeLoggedIn.getPassword().equals(DigestUtils.sha1Hex(oldPasswordTf.getText())));
 		return employeeLoggedIn.getPassword().equals(oldPassword);
 	}
 
 	private boolean isNewPasswordFirstSameAsNewPasswordSecond(String firstNewPassword, String secondNewPassword) {
-		System.out.println(employeeLoggedIn.toString());
 		return firstNewPassword.equals(secondNewPassword);
+	}
+
+	private boolean isOldPasswordAndNewPasswordSame(String old, String newFirst, String newSecond) {
+		String hashedPassword = DigestUtils.sha1Hex(newFirst);
+		return (old.equals(hashedPassword) && isNewPasswordFirstSameAsNewPasswordSecond(newFirst, newSecond));
+	}
+
+	private void addWrongDastaStyle(PasswordField passwordfield) {
+		passwordfield.getStyleClass().add("wrong-input-background");
+	}
+
+	private void removeWrongDastaStyle(PasswordField passwordfield) {
+		passwordfield.getStyleClass().remove("wrong-input-background");
 	}
 
 	private void closeWindow() {
